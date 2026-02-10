8e78b52c2fbbf36d42ce2ab7f773ca2d628fe891
IrynaKr1 <krutsiryna@gmail.com>
2026-02-10 20:20:14 +0100

    Add new Phone

---
+++ index.js
@@ -0,0 +1,20 @@
+const { sequelize, Phone } = require('./models');
+
+//додавання нового телефону
+(async function () {
+  const newPhone = {
+    brand: 'Apple',
+    model: 'iPhone 15 Pro Max',
+    manufacturedYear: '2023-09-22',
+    ram: 8,
+    cpu: 6,
+    screenSize: 6.7,
+    isNfc: true,
+  };
+
+  const createdPhone = await Phone.create(newPhone);
+  console.log('createdPhone', createdPhone.get());
+})();
+
+// отримання списку телефонів 
+// (* 3-я сторінка при перегляді по 4 телефони на сторінці, упорядкованих за роком виробництва)
