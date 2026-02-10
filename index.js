78cf09c08d3de0e6dfece7e1e188a64a371317d1
IrynaKr1 <krutsiryna@gmail.com>
2026-02-10 20:39:53 +0100

    Filter phones, ordering by manufacturedYear

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const { sequelize, Phone } = require('./models');
 
 //додавання нового телефону
+// (async function () {
+//   const newPhone = {
+//     brand: 'Apple',
+//     model: 'iPhone 15 Pro Max',
+//     manufacturedYear: '2023-09-22',
+//     ram: 8,
+//     cpu: 6,
+//     screenSize: 6.7,
+//     isNfc: true,
+//   };
+//   const createdPhone = await Phone.create(newPhone);
+//   console.log('createdPhone', createdPhone.get());
+// })();
+//
+//
+
+// отримання списку телефонів
+// (* 3-я сторінка при перегляді по 2 телефони на сторінці, упорядкованих за роком виробництва)
 (async function () {
-  const newPhone = {
-    brand: 'Apple',
-    model: 'iPhone 15 Pro Max',
-    manufacturedYear: '2023-09-22',
-    ram: 8,
-    cpu: 6,
-    screenSize: 6.7,
-    isNfc: true,
-  };
-
-  const createdPhone = await Phone.create(newPhone);
-  console.log('createdPhone', createdPhone.get());
+  const foundPhones = await Phone.findAll({
+    raw: true,
+    exclude: ['createdAt', 'updatedAt'],
+    limit: 2,
+    offset: 4,
+    order: [['manufacturedYear', 'DESC']],
+  });
+  console.log('foundPhones', foundPhones);
 })();
-
-// отримання списку телефонів 
-// (* 3-я сторінка при перегляді по 4 телефони на сторінці, упорядкованих за роком виробництва)
