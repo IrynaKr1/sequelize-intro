c26d87b8fe5b24a72c89020da7116d44efda1040
IrynaKr1 <krutsiryna@gmail.com>
2026-02-10 21:10:28 +0100

    findAll phones where manufacturedYear = 2026

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { sequelize, Phone } = require('./models');
+const { Op } = require('sequelize');
 
 //додавання нового телефону
 // (async function () {
 //   const newPhone = {
 //     brand: 'Apple',
-//     model: 'iPhone 15 Pro Max',
-//     manufacturedYear: '2023-09-22',
+//     model: 'iPhone 17',
+//     manufacturedYear: '2026-01-22',
 //     ram: 8,
-//     cpu: 6,
-//     screenSize: 6.7,
+//     cpu: 8,
+//     screenSize: 6.2,
 //     isNfc: true,
 //   };
 //   const createdPhone = await Phone.create(newPhone);
@@ -19,13 +20,26 @@ const { sequelize, Phone } = require('./models');
 
 // отримання списку телефонів
 // (* 3-я сторінка при перегляді по 2 телефони на сторінці, упорядкованих за роком виробництва)
-(async function () {
-  const foundPhones = await Phone.findAll({
-    raw: true,
-    exclude: ['createdAt', 'updatedAt'],
-    limit: 2,
-    offset: 4,
-    order: [['manufacturedYear', 'DESC']],
-  });
-  console.log('foundPhones', foundPhones);
-})();
+// (async function () {
+//   const foundPhones = await Phone.findAll({
+//     attributes: {
+//   exclude: ['createdAt', 'updatedAt'],
+// },
+//     limit: 2,
+//     offset: 4,
+//     order: [['manufacturedYear', 'DESC']],
+//   });
+//   console.log('foundPhones', foundPhones);
+// })();
+
+//*отримання списку телефонів поточного року видання,
+// (async function () {
+//   const foundPhones = await Phone.findAll({
+//     raw: true,
+//     attributes: {
+//       exclude: ['createdAt', 'updatedAt'],
+//     },
+//     where: sequelize.literal('EXTRACT(YEAR FROM "manufacturedYear") = 2026'),
+//   });
+//   console.log('foundPhones', foundPhones);
+// })();
