bf9a65bd2dbfddbd03ecf19ea757e848b15b7b95
IrynaKr1 <krutsiryna@gmail.com>
2026-02-22 12:18:51 +0100

    Add createdPhone

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ app.use(express.json());
 
 app.use('/api', router);
 
-// Add endpoints handlers
-
-// POST /api/phones {}
-// GET /api/phones
-// GET /api/phones/1
-// PATCH /api/phones/1 {}
-// DELETE /api/phones/1
-
 app.use(errorHandlers.errorHandler);
 
 module.exports = app;
--- controllers/phonesController.js
+++ controllers/phonesController.js
@@ -1,4 +1,18 @@
-module.exports.createdPhone = async (req, res, next) => {};
+const { Phone } = require('./../models');
+
+module.exports.createdPhone = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const createPhone = await Phone.create(body);
+    if (!createPhone) {
+      return res.status(400).send('Something went wrong');
+    }
+
+    res.status(201).send(createPhone);
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports.getPhone = async (req, res, next) => {};
 module.exports.getPhoneById = async (req, res, next) => {};
 module.exports.updatePhoneById = async (req, res, next) => {};
