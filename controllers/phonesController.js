ef74ed080e5a2bcf92ab67eb9cb4f7adce298094
IrynaKr1 <krutsiryna@gmail.com>
2026-02-22 12:30:10 +0100

    Add getPhone

--- controllers/phonesController.js
+++ controllers/phonesController.js
@@ -13,7 +13,19 @@ module.exports.createdPhone = async (req, res, next) => {
     next(error);
   }
 };
-module.exports.getPhone = async (req, res, next) => {};
+module.exports.getPhone = async (req, res, next) => {
+  try {
+    const founPhones = await Phone.findAll({
+      raw: true,
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+    res.status(200).send(founPhones);
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports.getPhoneById = async (req, res, next) => {};
 module.exports.updatePhoneById = async (req, res, next) => {};
 module.exports.deletePhoneById = async (req, res, next) => {};
